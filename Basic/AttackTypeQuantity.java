139f119158822782519c385e8cda90011bdd4bd8
unknown <luizhmatoso1340@gmail.com>
2025-03-26 22:41:34 -0300

    Add AttackTypeQuantity basic task

---
+++ Basic/AttackTypeQuantity.java
@@ -0,0 +1,77 @@
+package basic;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.log4j.BasicConfigurator;
+
+import java.io.IOException;
+
+public class AttackTypeQuantity {
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+        BasicConfigurator.configure();
+
+        Configuration c = new Configuration();
+        String[] files = new GenericOptionsParser(c, args).getRemainingArgs();
+
+        Path input = new Path("in/cybersecurity.csv");
+        Path output = new Path("output/attack_quantity_by_target_industry");
+
+        Job j = new Job(c, "attack_quantity_by_target_industry");
+
+        j.setMapperClass(AttackTypeQuantity.MapForAttackTypeQuantity.class);
+        j.setReducerClass(AttackTypeQuantity.ReduceForAttackTypeQuantity.class);
+
+        j.setMapOutputKeyClass(Text.class);
+        j.setMapOutputValueClass(IntWritable.class);
+
+        j.setOutputKeyClass(Text.class);
+        j.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(j, input);
+        FileOutputFormat.setOutputPath(j, output);
+
+        System.exit(j.waitForCompletion(true) ? 0 : 1);
+    }
+
+
+    public static class MapForAttackTypeQuantity extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+            String[] tokens = value.toString().split(",");
+
+            if (tokens.length > 2) {
+                String attackType = tokens[2].trim();
+                context.write(new Text(attackType), new IntWritable(1));
+            }
+
+        }
+
+    }
+
+    public static class ReduceForAttackTypeQuantity extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+            if (key.toString().equals("Attack Type")) {
+                return;
+            }
+
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+}
