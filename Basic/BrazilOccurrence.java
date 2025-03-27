81296106afe21f06abf6b25d378ed139015588cc
unknown <luizhmatoso1340@gmail.com>
2025-03-26 22:32:55 -0300

    Add BrazilOccurrence basic task

---
+++ Basic/BrazilOccurrence.java
@@ -0,0 +1,73 @@
+package basic;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.log4j.BasicConfigurator;
+
+import java.io.IOException;
+
+public class BrazilOccurrence {
+
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+
+        BasicConfigurator.configure();
+
+        Configuration c = new Configuration();
+        String[] files = new GenericOptionsParser(c, args).getRemainingArgs();
+
+        Path input = new Path("in/cybersecurity.csv");
+        Path output = new Path("output/brazil_occurrence");
+
+        Job j = new Job(c, "brazil_occurrence");
+
+        j.setMapperClass(MapForBrazilOccurrence.class);
+        j.setReducerClass(ReduceForBrazilOccurrence.class);
+
+        j.setMapOutputKeyClass(Text.class);
+        j.setMapOutputValueClass(IntWritable.class);
+
+        j.setOutputKeyClass(Text.class);
+        j.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(j, input);
+        FileOutputFormat.setOutputPath(j, output);
+
+        System.exit(j.waitForCompletion(true) ? 0 : 1);
+
+    }
+
+    public static class MapForBrazilOccurrence extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String[] tokens = value.toString().split(",");
+
+            for (String token : tokens) {
+                if (token.equals("Brazil")) {
+                    context.write(new Text(token), new IntWritable(1));
+                }
+            }
+        }
+
+    }
+
+    public static class ReduceForBrazilOccurrence extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+}
